d466fee7a24e085a21479b7ff1101771b392fc7b
Faraz Noor <faraznoor75@gmail.com>
2021-10-30 18:40:16 +0530

    Bring everything together for the magic.

---
+++ app/backend.rb
@@ -0,0 +1,23 @@
+require_relative './models'
+
+class Backend
+  attr_accessor :rules_list, :customer_purchases
+
+  def initialize
+    self.rules_list = []
+    self.customer_purchases = []
+    rules_list << HigherThanXRule.new
+    rules_list << TodayIsCertainDayOfTheMonthRule.new
+    rules_list << SecondPurchaseWithinXDaysRule.new
+  end
+
+  def get_reward(new_record)
+    reward = nil
+    rules_list.reverse.each do |rule|
+      reward = rule.apply(new_record, existing_memory: customer_purchases)
+      break unless reward.nil?
+    end
+    customer_purchases << new_record
+    reward
+  end
+end
